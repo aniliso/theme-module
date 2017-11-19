0f41f97345a2704cdd11f21358fe8269c47c86fb
sagsoz06 <sagsoz06@gmail.com>
2017-11-20 02:07:22 +0300

    Added responsive presenter settings

--- Presenters/SliderPresenter.php
+++ Presenters/SliderPresenter.php
@@ -6,6 +6,28 @@ class SliderPresenter extends BasePresenter
 {
     protected $zone     = 'sliderImage';
 
+    public function font_size()
+    {
+        if(isset($this->entity->settings->title_font_size) && isset($this->entity->settings->title_font_responsive)) {
+            $rangeSize = ($this->entity->settings->title_font_size - $this->entity->settings->title_font_responsive) / 4;
+            $titleFontSizes = range($this->entity->settings->title_font_size, $this->entity->settings->title_font_responsive, $rangeSize);
+            return $titleFontSizes = "['".implode("','", $titleFontSizes)."']";
+        } else {
+            return $titleFontSizes = "['50','40','30','20']";
+        }
+    }
+
+    public function line_height()
+    {
+        if(isset($this->entity->settings->title_line_height) && isset($this->entity->settings->title_font_responsive)) {
+            $rangeSize = ($this->entity->settings->title_line_height - $this->entity->settings->title_font_responsive) / 4;
+            $titleLineHeights = range($this->entity->settings->title_line_height, $this->entity->settings->title_font_responsive, $rangeSize);
+            return $titleLineHeights = "['".implode("','", $titleLineHeights)."']";
+        } else {
+            return $titleLineHeights = "['74','66','46','36']";
+        }
+    }
+
     public function link()
     {
         $link = new \stdClass();
